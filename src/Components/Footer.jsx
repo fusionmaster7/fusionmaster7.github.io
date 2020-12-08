d3acbbb8fd6cbd99cc006625af9ac5b9f6de81c7
Hardik Singh <hardiksingh297@gmail.com>
2020-12-08 19:05:46 +0530

    Added Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Made with <span>{"\u2764"}</span> in React.js
+    </div>
+  );
+};
+
+export default Footer;
