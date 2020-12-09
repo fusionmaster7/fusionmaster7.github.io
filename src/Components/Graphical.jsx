a68d0b70df771bf47e53d97de9c04b85864091e6
Hardik Singh <hardiksingh297@gmail.com>
2020-12-09 23:58:39 +0530

    Added Projects

--- src/Components/Graphical.jsx
+++ src/Components/Graphical.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
+import Projects from "./Projects";
 
 const Graphical = () => {
-  return <h1 style={{ color: "white" }}>Graphical here</h1>;
+  const [view, setView] = useState("info");
+
+  const renderView = () => {
+    switch (view) {
+      case "info":
+        return <div className="gui-heading">Info</div>;
+      case "skills":
+        return <div className="gui-heading">Skills</div>;
+      case "projects":
+        return <Projects />;
+      default:
+        return <div className="gui-heading">Info</div>;
+    }
+  };
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    const choice = e.target.innerHTML.toLowerCase();
+    setView(choice);
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="gui-btn-row">
+        <div>
+          <button className="gui-btn" onClick={clickHandler}>
+            Info
+          </button>
+        </div>
+        <div>
+          <button className="gui-btn" onClick={clickHandler}>
+            Skills
+          </button>
+        </div>
+        <div>
+          <button className="gui-btn" onClick={clickHandler}>
+            Projects
+          </button>
+        </div>
+      </div>
+      <div>{renderView()}</div>
+    </React.Fragment>
+  );
 };
 
 export default Graphical;
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import projects from "../projects.json";
+
+const Card = ({ project }) => {
+  const renderStack = () => {
+    return project.stack.map((tech, index) => {
+      return (
+        <div className="tech" key={index}>
+          {tech}
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="project-card">
+      <div className="project-title">{project.name}</div>
+      <hr className="project-sep" />
+      <div className="project-desc">{project.desc}</div>
+      <div className="tech-stack">{renderStack()}</div>
+    </div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <div className="project-container">
+      <div className="project-row">
+        <Card project={projects[0]} />
+        <Card project={projects[1]} />
+        <Card project={projects[2]} />
+      </div>
+      <div className="project-row">
+        <Card project={projects[3]} />
+        <Card project={projects[4]} />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
