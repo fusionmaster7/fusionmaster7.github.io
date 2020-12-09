7c9d78da296ea03e2bcb39966f7bc587c36c582b
Hardik Singh <hardiksingh297@gmail.com>
2020-12-09 23:58:26 +0530

    Removed Header

--- src/Components/Terminal.jsx
+++ src/Components/Terminal.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from "react";
-import Header from "./Header";
 import commandParser from "../messages.util";
 import { Context } from "../Store/Context";
 
