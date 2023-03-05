0acbb9d82e355e104eeacfb308a2176020d01c62
fusionmaster7 <hardiksingh297@gmail.com>
2023-03-06 02:23:19 +0530

    Basic site layout completed

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Working", Date.now());
+let MODE = "";
+
+function setMode() {
+  const { host } = window.location;
+  MODE = host === "localhost" ? "DEV" : "PROD";
+}
+
+function getBaseUrl() {
+  if (MODE === "DEV") {
+    return "localhost:5500";
+  } else {
+    return "https://fusionmaster7.github.io";
+  }
+}
